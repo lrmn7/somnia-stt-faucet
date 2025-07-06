1ad6de19b5f5193c7571d652ecdd41c8fb0c498a
lrmn7 <lrmn.dev@gmail.com>
2025-07-06 18:05:17 +0700

    chore: add rewrite for public quest verification endpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/event/verify-player/:address',
+        destination: '/api/event/verify-player/:address',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
