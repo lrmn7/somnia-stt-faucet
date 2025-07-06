5a23110ea243d3043db94dfda5970d42f68b4a05
lrmn7 <lrmn.dev@gmail.com>
2025-07-06 17:48:29 +0700

    remove claim reward

--- src/components/game/GameOverScreen.tsx
+++ src/components/game/GameOverScreen.tsx
@@ -2,50 +2,19 @@ interface GameOverScreenProps {
   currentScore: number;
   onResetGame: () => void;
   isSavingScore: boolean;
-  onClaimReward: () => void;
-  isClaimingReward: boolean;
-  hasClaimedReward: boolean;
 }
 
 const GameOverScreen = ({
   currentScore,
   onResetGame,
   isSavingScore,
-  onClaimReward,
-  isClaimingReward,
-  hasClaimedReward,
 }: GameOverScreenProps) => {
-  const isButtonDisabled = isSavingScore || isClaimingReward;
-
-  if (hasClaimedReward) {
-    return (
-      <div className="mt-8 p-6 bg-dark-secondary rounded-lg shadow-xl w-full max-w-xl">
-        <h2 className="text-3xl font-bold text-dark-accent mb-4">
-          You’ve Reached the End!
-        </h2>
-        <p className="text-xl mb-2">
-          Your final score{" "}
-          <span className="font-bold text-dark-accent">{currentScore}</span>{" "}
-          Points
-        </p>
-        <p className="text-md text-dark-text-secondary mb-6">
-          Reward already claimed. Thank you for playing!
-        </p>
-        <button
-          onClick={onResetGame}
-          className="px-6 py-2 bg-dark-accent text-dark-primary font-semibold rounded-lg hover:bg-opacity-80 transition-colors"
-        >
-          See you!
-        </button>
-      </div>
-    );
-  }
-
   return (
     <div className="mt-8 p-6 bg-dark-secondary rounded-lg shadow-xl w-full max-w-xl">
       <h2 className="text-3xl font-bold text-dark-accent mb-4">
         You’ve Reached the End!
       </h2>
+
       <p className="text-xl mb-2">
         Your final score{" "}
         <span className="font-bold text-dark-accent">{currentScore}</span>{" "}
@@ -54,23 +23,23 @@ const GameOverScreen = ({
 
       <p className="text-md text-dark-text-secondary mb-6">
         {isSavingScore ? (
-          "Updating score..."
+          "Please wait..."
         ) : (
           <>
-            All quizzes are done! <br />
-            Check the leaderboard, are you one of the Somnia Legends? <br />
-            See you in the next season!
+            You’ve completed the quiz! See how you rank on the leaderboard. 🎉
           </>
         )}
       </p>
+
       <button
-        onClick={onClaimReward}
-        disabled={isButtonDisabled}
-        className="px-6 py-2 bg-dark-accent text-dark-primary font-semibold rounded-lg hover:bg-opacity-80 transition-colors disabled:opacity-50 mr-4"
+        onClick={onResetGame}
+        disabled={isSavingScore}
+        className="px-6 py-2 bg-dark-accent text-dark-primary font-semibold rounded-lg hover:bg-opacity-80 transition-colors disabled:opacity-50"
       >
-        {isSavingScore ? "Please wait..." : "Claim Rewards 0.2 STT"}
+        Back to Home
       </button>
     </div>
   );
 };
+
 export default GameOverScreen;
