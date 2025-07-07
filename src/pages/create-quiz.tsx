fed6fe0e95f8792e49a178ea7bcd237971e79b5e
lrmn7 <lrmn.dev@gmail.com>
2025-07-07 07:39:39 +0700

    chore: add rewrite for public quest verification endpoint

--- src/pages/create-quiz.tsx
+++ src/pages/create-quiz.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import MainLayout from "@/components/layout/MainLayout";
 
 const CreateQuizPage: NextPage = () => {
@@ -25,6 +26,11 @@ const CreateQuizPage: NextPage = () => {
           className="rounded-xl"
         />
       </div>
+            <Link href="/" legacyBehavior>
+        <a className="mt-8 px-6 py-2 bg-dark-accent text-dark-primary font-semibold rounded-lg hover:bg-opacity-80 transition-colors">
+          Back to Home
+        </a>
+      </Link>
     </MainLayout>
   );
 };
